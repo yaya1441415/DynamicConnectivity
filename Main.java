3d57b715f8d72be4d54f58801f8117f708e75b97
yaya1441415 <135056178+yaya1441415@users.noreply.github.com>
2023-08-27 22:43:22 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        boolean[][] grid = new boolean[3][3];
-        grid[2][2]=true;
-        System.out.println(Arrays.toString(grid[2]));
-    }
-}
